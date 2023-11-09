0c6e405b8f17e51a33e9cf037232b4b911afd887
Un1Cornn <veron.klop@gmail.com>
2023-11-09 23:01:45 +0100

    zxc

--- fetch_github_data.py
+++ fetch_github_data.py
@@ -1,5 +1,7 @@
 import re, json, requests 
 
+app = fetch_github_data(__name__)
+
 url = 'https://raw.githubusercontent.com/Un1Cornn/vireonsky/main/test.json'
 resp = requests.get(url)
 poop = json.loads(resp.text)
